e3244139981f09a42f8725d2dc0ae95b1fd55d4f
MrNity <nitayl@list.ru>
2022-09-14 21:43:55 +0300

    ok

--- api/app.js
+++ api/app.js
@@ -9,10 +9,14 @@ const https             = require('https')
 const axios             = require('axios')
 const mongoose          = require('mongoose')
 const crypto            = require('crypto')
+const fse               = require('fs-extra')
+const TelegramBot       = require('node-telegram-bot-api')
 
 // -----------------------------
 // FILES
 const config            = require('./config')
+const fileCids          = './cids.json'
+const TOKEN             = config.TELEGRAM_BOT
 
 // -----------------------------
 // CONSTANTSр
@@ -39,6 +43,107 @@ let LINKS = []
 //setInterval(function() {
 //    console.log({LINKS})
 //}, 1000)
+
+// -----------------------------
+// BOT
+const bot = new TelegramBot(`${TOKEN}`, {polling: true})
+// -----------------------------
+
+const help = `Доступные команды:\n\n/on - Включить оповещения\n/off - Выключить оповещения`
+const link = `https://genshin.hoyoverse.com/ru/gift?code=`
+
+// COMANDS
+bot.onText(/\/myid/, (msg) => {
+    const cid = msg.chat.id
+    sendHTML(cid, `Ваш <b>CHAT ID: ${cid}</b>`)
+})
+bot.onText(/\/on/, (msg) => {
+    let chatId = cid(msg)
+    fse.ensureFile(fileCids).then(() => {
+        fse.readJson(fileCids).then(text => {
+            let cids = text.cids
+            let cidIndex = cids.indexOf(chatId)
+            if (cidIndex != -1) {
+                sendHTML(chatId, `Уведомления уже <b>включены</b>!\n\nЧтобы выключить нажмите /off`)
+            } else {
+                cids.push(chatId)
+                fs.writeFile(fileCids, JSON.stringify({cids}), function (err) {
+                    if (err) {
+                        newError(error)
+                    }
+                    sendHTML(chatId, `Уведомления <b>включены</b>!\n\nЧтобы выключить обратно /off`)
+                })
+            }
+        })
+    })
+})
+bot.onText(/\/off/, (msg) => {
+    let chatId = cid(msg)
+    fse.ensureFile(fileCids).then(() => {
+        fse.readJson(fileCids).then(text => {
+            let cids = text.cids
+            let cidIndex = cids.indexOf(chatId)
+            if (cidIndex != -1) {
+                cids.splice(cidIndex, 1)
+                fs.writeFile(fileCids, JSON.stringify({cids}), function (err) {
+                    if (err) {
+                        newError(error)
+                    }
+                    sendHTML(chatId, `Уведомления <b>выключены</b>!\n\nЧтобы выключить обратно /on`)
+                })
+            } else {
+                sendHTML(chatId, `Уведомления уже <b>выключены</b>!\n\nЧтобы включить нажмите /on`)
+            }
+        })
+    })
+})
+bot.onText(/\/start/, msg => {
+    let chatId = cid(msg)
+    let name = msg.from.first_name
+    
+    sendHTML(chatId, `Добро пожаловать!\n${help}`)
+})
+bot.onText(/\/code/, (msg, [source, match]) => {
+    let chatId = cid(msg)
+//    \s(\w+[^\d+])
+//    let message = msg.text
+//    let sss = message.substr(6, message.length)
+//    let code = sss.match(/(\w+[^\d+])/)[0]
+//    let sd = message.substr(6+code.length, message.length)
+//    let date = sd.match(/\d/)
+    bot.sendMessage(chatId, `Введите код!`).then(() => {
+        GetText(chatId).then(code => {
+//            console.log({code})
+            if (code != '-') {
+                fse.ensureFile(fileCids).then(() => {
+                    fse.readJson(fileCids).then(text => {
+                        let cids = text.cids
+
+                        cids.forEach(id => {
+                            sendHTML(id, `Новый промокод!\n\n<a href="${link}${code}">${code}</a>`)
+                        })
+                    })
+                })
+            }
+
+        })
+    })
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // -----------------------------
@@ -79,6 +184,12 @@ app.disable('x-powered-by')
 app.post('/signin', function (req, res) {
     let username = req.body.username
     let password = Crypt(req.body.password)
+    
+    
+    
+    
+    
+    
     
 })
 
@@ -159,6 +270,43 @@ function GetDateTime(date) {
     let second      = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
     
     return `${day}.${month}.${year} ${hour}:${minute}:${second}`
+}
+
+
+// FUNCTIONS FOR BOT
+function cid(msg) { return msg.chat.id }
+function GetText(cid) {
+    return new Promise(function(resolve, reject) {
+        bot.once('message', (msg) => {
+            let text = msg.text
+            let cid_2 = msg.chat.id
+            if (cid == cid_2) {
+                resolve(text)
+            } else {
+                GetText(cid).then(text_2 => {
+                    resolve(text_2)
+                })
+            }
+        })
+    })
+}
+
+function sendHTML(chatId, html) {
+    const options = {
+        parse_mode: 'HTML'
+    }
+    bot.sendMessage(chatId, html, options)
+}
+
+function sendHTML(chatId, html) {
+    return new Promise(function(resolve, rejext) {
+        const options = {
+            parse_mode: 'HTML'
+        }
+        bot.sendMessage(chatId, html, options).then(() => {
+            resolve()
+        })
+    })
 }
 
 // FUNCTIONS GET
--- api/config.js
+++ api/config.js
@@ -2,6 +2,7 @@ module.exports = {
     PORT: 8080,
     HTTPS_PORT: 9090,
     DB_URL: `mongodb://localhost/nyanpasu`,
+    TELEGRAM_BOT: `5384695994:AAFXaZrNjAZxkAX-t2NkCFoMGUbN-FHzEkw`,
     session: {
         secret: ['nyanpasu lol kekeekekekekek', '04ab4cd50aa14c18d001fe46d8dda70c4a5f4c972daf296b972221cf0371a438b521f0a79bf0b8fdebcd38fa503e0a4e2f148368e087d2a992b9cca6491fbc78', 'd7256f5b14f87e71f998f946060c1ebf2667292b38049be8f6074586ad3cf8dc'],
         key: 'nyan pas pasu kek keyuo',
